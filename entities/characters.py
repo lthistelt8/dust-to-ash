d4c9f3c5fadab672e05f441029b146f96ce866ac
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-20 10:25:32 -0400

    initialized base class Character, subclasses PlayerCharacter and EnemyCharacter

---
+++ entities/characters.py
@@ -0,0 +1,29 @@
+class Character():
+    '''Methods shared across enemies and players'''
+    def __init__(self, name, health=1, attack=1, defense=1, speed=1):
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+    
+    def take_damage(self, damage):
+        '''Take damage, reducing health'''
+        self.health -= damage
+        self.health = max(self.health, 0)
+
+    def attack_enemy(self, enemy):
+        '''Attack enemy based on health and defense'''
+        damage = max(0, self.attack - enemy.defense)
+        enemy.take_damage(damage)
+        return damage
+
+class PlayerCharacter(Character):
+    '''Player character methods'''
+    super(Character)
+    pass
+
+class EnemyCharacter(Character):
+    '''Enemy character methods'''
+    super(Character)
+    pass
