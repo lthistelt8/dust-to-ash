d319b36561310dae7766bdaa6f2a03096e451998
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-22 12:53:31 -0400

    added module docstring, initialized combat loop menu

---
+++ battle/combat_loop.py
@@ -0,0 +1,26 @@
+'''Controls everything to do with the combat itself'''
+
+from battle import attack_types, enemy_loot, turn_order
+from entities import player_characters, enemy_characters
+
+
+
+def combat_loop(char1, enemy1):
+    '''Combat loop'''
+    inCombat = True
+
+    while inCombat:
+        turn_order.get_turn_order(char1, enemy1)
+        action_list = ['Attack', 'Item']
+        for a, action in enumerate(action_list, 1):
+            print(f"{a}. {action}.")
+
+        print("What would you like to do?")
+        try:
+            choice = int(input("> "))
+        except ValueError:
+            print("Must choose from the listed numbers!")
+            return None
+        
+        if choice == 1:
+            char1.
