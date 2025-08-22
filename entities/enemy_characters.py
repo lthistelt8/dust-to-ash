7088bd3d1954af06f7034d2a2747660fafb26f8e
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-22 02:20:56 -0400

    imported ATTACK_TYPES for player and enemy character modules

--- entities/enemy_characters.py
+++ entities/enemy_characters.py
@@ -1,3 +1,4 @@
 from characters import EnemyCharacter
+from battle.attack_types import ATTACK_TYPES
 
 enemy1 = EnemyCharacter('enemy1')
--- entities/player_characters.py
+++ entities/player_characters.py
@@ -1,3 +1,4 @@
 from characters import PlayerCharacter
+from battle.attack_types import ATTACK_TYPES
 
 char1 = PlayerCharacter('char1')
