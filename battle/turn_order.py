8db68b680e68c14304985d9e31257ad1b774f88c
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-22 12:44:58 -0400

    added variable arguments, deindented trailing whitespace

--- battle/turn_order.py
+++ battle/turn_order.py
@@ -1,9 +1,9 @@
 '''Controls turn order in combat'''
 from entities import enemy_characters, player_characters
 
-def get_turn_order(fighters):
+def get_turn_order(*fighters):
     '''Returns turn order'''
-    
+
     fighters = [player_characters.char1] + [enemy_characters.enemy1]
 
     turn_order = sorted(fighters, key= lambda c: c.speed, reverse=True)
