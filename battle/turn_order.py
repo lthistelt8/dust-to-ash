5d73a15b4bd3268db1f9039fc5965ffff783e651
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-22 10:53:16 -0400

    created function to generate turn order in combat

--- battle/turn_order.py
+++ battle/turn_order.py
@@ -1,8 +1,12 @@
 '''Controls turn order in combat'''
+from entities import enemy_characters, player_characters
 
-from attack_types import ATTACK_TYPES
-inCombat: bool = False
+def get_turn_order(fighters):
+    '''Returns turn order'''
+    inCombat: bool = False
 
-if inCombat == True:
-    #entity with highest speed goes first
-    pass
+    if inCombat is True:
+        fighters = player_characters.char1 + enemy_characters.enemy1
+
+        turn_order = sorted(fighters, key= lambda c: c.speed, reverse=True)
+        return turn_order
