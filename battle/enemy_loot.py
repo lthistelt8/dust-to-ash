af08dff3d029487a7611ed645bf4c6c35a1792ba
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-22 02:41:25 -0400

    initialized enemy loot module

---
+++ battle/enemy_loot.py
@@ -0,0 +1,14 @@
+'''A dict of dicts of enemy loot, based on the enemy type'''
+
+ENEMY_LOOT = {
+    'Enemy1': {
+        'item1',
+        'item2'
+    }
+}
+
+BOSS_LOOT = {
+    'MB1': {
+        'MBitem1'
+    }
+}
