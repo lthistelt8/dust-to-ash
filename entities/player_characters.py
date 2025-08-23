ff61c02f0b6854dd9d1ea138587fee360edb69ea
lthistelt8 <o_lthistel6@protonmail.com>
2025-08-23 10:47:05 -0400

    added default attack type if attack type not in ATTACK_TYPES dict

--- entities/player_characters.py
+++ entities/player_characters.py
@@ -4,4 +4,8 @@ from characters import PlayerCharacter
 from battle.attack_types import ATTACK_TYPES
 
 char1 = PlayerCharacter('char1')
+char1.attack_type = 'blunt'
+if char1.attack_type not in ATTACK_TYPES:
+    char1.attack_type = 'strike'
 
+print(char1)
